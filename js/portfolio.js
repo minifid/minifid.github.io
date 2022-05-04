e4990b645a244052ea7641ab71a06830584ac6b9
MiniFid <nathan.d.fidler@gmail.com>
2022-05-04 13:04:24 -0600

    added js

---
+++ js/portfolio.js
@@ -0,0 +1,46 @@
+// These functions open and close the contact form
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+// initiates slide
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Changes slide to the next or previous slide
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Changes slide to specific slide
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+//displays current slide 
+function showSlides(n) {
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}; 
+    if (n < 1) {slideIndex = slides.length};
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+
+
+document.addEventListener("click", function(event){
+    if (event.target.matches(".cancel") || !event.target.closest(".form-popup") && !event.target.closest(".Pop_Up_Button") && !event.target.closest(".contact")){
+        closeForm()
+    }
+}, false )
